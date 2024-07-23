382f8db0a75a31aea8e21a3f5c9036e046bdef32
UNYIME INOH <40715735+Unyimesax@users.noreply.github.com>
2024-07-23 06:07:00 +0000

    Added codes to complete the Calculator project.

--- main.py
+++ main.py
@@ -22,18 +22,31 @@ operations = {
     
 }
 
-num1 = int(input("What's the first number?: \n"))
-
-for symbol in operations:
-    print(symbol)
-
-operation_symbol = input("choose an operation from the line above: \n ")
-
-num2 = int(input("What's the second number?: \n"))
-
-calculation_function = operations[operation_symbol]
-
-answer = calculation_function(num1, num2)
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
-
+def calculator():
+    num1 = float(input("What's the first number?: \n"))
+    
+    for symbol in operations:
+        print(symbol)
+    
+    should_continue = True
+    
+    while should_continue:
+        
+        operation_symbol = input("choose an operation: \n ")
+        
+        num2 = float(input("What's the next number?: \n"))
+        
+        calculation_function = operations[operation_symbol]
+        
+        answer = calculation_function(num1, num2)
+        
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        
+        continue_calc = input(f"Type 'y' to continue calculating with {answer} or type 'n' to restart :\n")
+        if continue_calc == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+    
+calculator()    
